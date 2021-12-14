a2e3f54262c552f6513d0073ac6b08db9c534d9f
itsuki <88591404+futureituki@users.noreply.github.com>
2021-12-15 01:23:12 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
+
+function App() {
+  return (
+    <>
+   <Header/>
+   <Skills/>
+   <Contact/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const requestStatus={
+  idle:'IDLE',
+  loading:'LOADING',
+  success:'SUCCESS',
+  error:'ERROR'
+}
